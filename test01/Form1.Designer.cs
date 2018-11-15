57c1c1288086c6d319c0341e91f4bc3ba95fccc2
sdustim <sdustim@163.com>
2018-11-15 21:28:41 +0800

    first version

---
+++ test01/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace test01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Green;
+            this.richTextBox1.Location = new System.Drawing.Point(2, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 306);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.TabStop = false;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 419);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 4;
+            this.button3.TabStop = false;
+            this.button3.Text = "字节数组转整数";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 387);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.TabStop = false;
+            this.button4.Text = "正则表达式";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(339, 83);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(469, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "abcd";
+            title1.Name = "Title1";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(521, 401);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.TabStop = false;
+            this.button5.Text = "chart";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(311, 415);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.TabStop = false;
+            this.button6.Text = "IniFile";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(448, 423);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 9;
+            this.button7.TabStop = false;
+            this.button7.Text = "List复制";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(415, 30);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.TabStop = false;
+            this.button8.Text = "SubString";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(570, 32);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "建目录";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 472);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ test01/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms.DataVisualization.Charting;
+using AppExtend1;
+
+namespace test01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+            //richTextBox1.SelectionColor = Color.Red;
+            //richTextBox1.Focus();
+            Blowfish bf = new Blowfish();
+            Byte[] aa = new Byte[8] {49,50,51,52,53,54,55,56 };
+            Byte[] bb = bf.EncryptByte(aa, 8);
+            richTextBox1.AppendText(bb.ToString());
+            Blowfish cf = new Blowfish();
+            Byte[] cc = cf.DecryptByte(bb, 24);
+            richTextBox1.AppendText(aa.ToString());
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("123ABCD" + "\r\n");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string filename = "检测报告" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            
+            StreamWriter sw = new StreamWriter(fs,Encoding.UTF8);
+            sw.WriteLine(filename);
+            sw.WriteLine("检测值, 下限, 上限, 判断结果");
+            Random rrr=new Random();
+            for(int i=0;i<10;i++)
+            {
+                 sw.WriteLine(rrr.NextDouble().ToString()+"," + rrr.NextDouble().ToString()+"," + rrr.NextDouble().ToString()+"," + rrr.NextDouble().ToString());
+            }
+            sw.Close();
+            fs.Close();
+            string ss;
+            ss = "共检测了10个点，其中5个异常，总体检测判断本电路板不合格";
+            ss += "检测报告见：" + Environment.CurrentDirectory + "\\" + filename;
+            richTextBox1.Text = ss;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*Byte[] bb=BitConverter.GetBytes(24);
+            foreach (Byte b in bb)
+                richTextBox1.AppendText(b.ToString("X2"));
+            richTextBox1.AppendText("\r\n");
+            int aa = BitConverter.ToInt32(bb, 0);
+            richTextBox1.AppendText(aa.ToString());
+            */
+            byte[] bb = { 0xFF, 0xA2, 0x44, 1, 0, 0, 0, 1 };
+            byte[] cc = { 0xFF, 0xA2 };
+            Array.Reverse(cc);
+            Int32 dd=BitConverter.ToInt16(cc,0);
+            //richTextBox1.AppendText(dd.ToString());
+            string ss = BitConverter.ToString(bb);
+            richTextBox1.AppendText(ss);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strall = File.ReadAllText("aaa.txt");
+            //string rgstr = GetMidStr(strall, "@00RD00", "*\r");
+            string[] strSperator = { "*\r","@00" };
+            string[] strarr = strall.Split(strSperator,System.StringSplitOptions.RemoveEmptyEntries);
+            foreach(string s in strarr)
+                richTextBox1.AppendText(s+"\r\n");
+        }
+        public string GetValue(string str, string s, string e)//正则表达式，取s ,e中间的字符串，好像不行
+        {
+            Regex rg = new Regex("(?<=(" + s + "))[.\\s\\S]*?(?=(" + e + "))", RegexOptions.Multiline | RegexOptions.Singleline);
+            return rg.Match(str).Value;
+        }
+        private string GetMidStr(string TxtStr, string FirstStr, string SecondStr)
+        {
+            if (FirstStr.IndexOf(SecondStr, 0) != -1)
+                return "";
+            int FirstSite = TxtStr.IndexOf(FirstStr, 0);
+            int SecondSite = TxtStr.IndexOf(SecondStr, FirstSite + 1);
+            if (FirstSite == -1 || SecondSite == -1)
+                return "";
+            return TxtStr.Substring(FirstSite + FirstStr.Length, SecondSite - FirstSite - FirstStr.Length);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            double[] num = new double[20];
+            Series series = chart1.Series[0];
+            for (int i = 0; i < 20; i++)
+            {
+                int valuey = rd.Next(20, 100);
+                DataPoint point = new DataPoint((i + 1), valuey);
+                series.Points.Add(point);
+            }
+            chart1.Titles[0].Text = "max:"+200.ToString()+"   min:100";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //AppExtend1.IniFile.WriteIniOneItem("test01.ini", "reginfo", "ABCD0924");
+            string s = IniFile.GetIniOneValue("test01.ini", "reginfo");
+            richTextBox1.AppendText(s);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            List<int> m_iniValuesList = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            int idex = m_iniValuesList.LastIndexOf(6);
+            List<int> listTmp = m_iniValuesList.GetRange(idex + 1, m_iniValuesList.Count - idex - 1);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string s = "1000025100008";
+            richTextBox1.AppendText(s.Substring(0, 7)+"\r\n");
+            richTextBox1.AppendText(s.Substring(7, 6));
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            richTextBox1.AppendText(e.KeyChar.ToString());
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(@".\img\");
+            richTextBox1.Text = "aaaa";
+        }
+    }
+}
